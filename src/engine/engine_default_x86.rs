525a58667e9aae5d2aa9392d4cb904f8c57760bc
Anders Trier Olesen <anders.trier.olesen@gmail.com>
2023-11-20 21:51:18 +0100

    Simplify DefaultEngine for x86

--- src/engine/engine_default_x86.rs
+++ src/engine/engine_default_x86.rs
@@ -1,52 +1,21 @@
-use once_cell::sync::OnceCell;
-
 use crate::engine::{Avx2, Engine, GfElement, NoSimd, ShardsRefMut, Ssse3, GF_ORDER};
 
 // ======================================================================
-// STATIC - PRIVATE
+// DefaultEngine - PUBLIC
 
-static BEST_ENGINE: OnceCell<InnerEngine> = OnceCell::new();
-
-// ======================================================================
-// FUNCTIONS - PRIVATE
-
-fn select_best_engine() -> InnerEngine {
-    if is_x86_feature_detected!("avx2") {
-        return InnerEngine::Avx2(Avx2::new());
-    }
-
-    if is_x86_feature_detected!("ssse3") {
-        return InnerEngine::Ssse3(Ssse3::new());
-    }
-
-    InnerEngine::NoSimd(NoSimd::new())
-}
-
-fn get_best_engine() -> &'static InnerEngine {
-    BEST_ENGINE.get_or_init(select_best_engine)
-}
-
-// ======================================================================
-// InnerEngine - PRIVATE
-
-enum InnerEngine {
-    NoSimd(NoSimd),
+/// [`Engine`] that on x86 platforms at runtime chooses the best Engine.
+#[derive(Clone)]
+pub enum DefaultEngine {
     Avx2(Avx2),
     Ssse3(Ssse3),
+    NoSimd(NoSimd),
 }
-
-// ======================================================================
-// DefaultEngine - PUBLIC
 
 impl Default for DefaultEngine {
     fn default() -> Self {
         Self::new()
     }
 }
-
-/// [`Engine`] that on x86 platforms at runtime chooses the best Engine.
-#[derive(Clone)]
-pub struct DefaultEngine();
 
 impl DefaultEngine {
     /// Creates new [`DefaultEngine`] by chosing and initializing the underlying engine.
@@ -56,8 +25,15 @@ impl DefaultEngine {
     /// 2. [`Ssse3`]
     /// 3. [`NoSimd`]
     pub fn new() -> Self {
-        get_best_engine();
-        Self()
+        if is_x86_feature_detected!("avx2") {
+            return DefaultEngine::Avx2(Avx2::new());
+        }
+
+        if is_x86_feature_detected!("ssse3") {
+            return DefaultEngine::Ssse3(Ssse3::new());
+        }
+
+        DefaultEngine::NoSimd(NoSimd::new())
     }
 }
 
@@ -70,21 +46,23 @@ impl Engine for DefaultEngine {
         truncated_size: usize,
         skew_delta: usize,
     ) {
-        let engine = get_best_engine();
-        match engine {
-            InnerEngine::NoSimd(e) => e.fft(data, pos, size, truncated_size, skew_delta),
-            InnerEngine::Avx2(e) => e.fft(data, pos, size, truncated_size, skew_delta),
-            InnerEngine::Ssse3(e) => e.fft(data, pos, size, truncated_size, skew_delta),
+        match self {
+            DefaultEngine::Avx2(e) => e.fft(data, pos, size, truncated_size, skew_delta),
+            DefaultEngine::Ssse3(e) => e.fft(data, pos, size, truncated_size, skew_delta),
+            DefaultEngine::NoSimd(e) => e.fft(data, pos, size, truncated_size, skew_delta),
         };
     }
 
     fn fwht(data: &mut [GfElement; GF_ORDER], truncated_size: usize) {
-        let engine = get_best_engine();
-        match engine {
-            InnerEngine::NoSimd(_) => NoSimd::fwht(data, truncated_size),
-            InnerEngine::Avx2(_) => Avx2::fwht(data, truncated_size),
-            InnerEngine::Ssse3(_) => Ssse3::fwht(data, truncated_size),
-        };
+        if is_x86_feature_detected!("avx2") {
+            return Avx2::fwht(data, truncated_size);
+        }
+
+        if is_x86_feature_detected!("ssse3") {
+            return Ssse3::fwht(data, truncated_size);
+        }
+
+        NoSimd::fwht(data, truncated_size)
     }
 
     fn ifft(
@@ -95,29 +73,30 @@ impl Engine for DefaultEngine {
         truncated_size: usize,
         skew_delta: usize,
     ) {
-        let engine = get_best_engine();
-        match engine {
-            InnerEngine::NoSimd(e) => e.ifft(data, pos, size, truncated_size, skew_delta),
-            InnerEngine::Avx2(e) => e.ifft(data, pos, size, truncated_size, skew_delta),
-            InnerEngine::Ssse3(e) => e.ifft(data, pos, size, truncated_size, skew_delta),
+        match self {
+            DefaultEngine::Avx2(e) => e.ifft(data, pos, size, truncated_size, skew_delta),
+            DefaultEngine::Ssse3(e) => e.ifft(data, pos, size, truncated_size, skew_delta),
+            DefaultEngine::NoSimd(e) => e.ifft(data, pos, size, truncated_size, skew_delta),
         };
     }
 
     fn mul(&self, x: &mut [u8], log_m: GfElement) {
-        let engine = get_best_engine();
-        match engine {
-            InnerEngine::NoSimd(e) => e.mul(x, log_m),
-            InnerEngine::Avx2(e) => e.mul(x, log_m),
-            InnerEngine::Ssse3(e) => e.mul(x, log_m),
-        }
+        match self {
+            DefaultEngine::Avx2(_) => Avx2::mul(x, log_m),
+            DefaultEngine::Ssse3(_) => Ssse3::mul(x, log_m),
+            DefaultEngine::NoSimd(_) => NoSimd::mul(x, log_m),
+        };
     }
 
     fn xor(x: &mut [u8], y: &[u8]) {
-        let engine = get_best_engine();
-        match engine {
-            InnerEngine::NoSimd(_) => NoSimd::xor(x, y),
-            InnerEngine::Avx2(_) => Avx2::xor(x, y),
-            InnerEngine::Ssse3(_) => Ssse3::xor(x, y),
-        };
+        if is_x86_feature_detected!("avx2") {
+            return Avx2::xor(x, y);
+        }
+
+        if is_x86_feature_detected!("ssse3") {
+            return Ssse3::xor(x, y);
+        }
+
+        NoSimd::xor(x, y)
     }
 }
