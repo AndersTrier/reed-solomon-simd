30235aca0ddf3ab5099e28653c29c0b50fe2a43a
AndersTrier <anders.trier.olesen@gmail.com>
2024-11-25 13:41:25 +0100

    impl ExactSizeIterator for RestoredOriginal

--- src/decoder_result.rs
+++ src/decoder_result.rs
@@ -93,6 +93,11 @@ impl<'a> Iterator for RestoredOriginal<'a> {
 }
 
 // ======================================================================
+// RestoredOriginal - IMPL ExactSizeIterator
+
+impl ExactSizeIterator for RestoredOriginal<'_> {}
+
+// ======================================================================
 // RestoredOriginal - CRATE
 
 impl<'a> RestoredOriginal<'a> {
